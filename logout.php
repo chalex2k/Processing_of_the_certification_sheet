c054c2e5030975a8ee668a53811db0009b82219f
linaShurygina <rocknrolllb20@gmail.com>
2019-08-20 15:12:55 +0300

    версия 1.0 для навигации

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+	if (isset($_SESSION['username']))
+	{
+		destroy_session();
+	}
+
+	header('Location: index.php');
+
+	function destroy_session()
+	{
+		$_SESSION = array();
+		if (session_id() != "" || isset($_COOKIE[session_name()]))
+			setcookie(session_name(), '', time() - 2592000, '/');
+		session_destroy();
+	}
+?>
