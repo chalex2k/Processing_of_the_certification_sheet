4f9909edad6947e4e103d963d9465b210cbc2c2d
linaShurygina <rocknrolllb20@gmail.com>
2019-08-21 17:32:28 +0300

    Основная страница студента

---
+++ stud_main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php 
+	$title = "Студент";
+	require_once 'head.php';
+?>
+</head>
+<body>
+	<div>
+		<a href="messages.php">Сообщения</a>
+	</div>
+	<div>
+		<a href="purposes.php">Цели</a>
+	</div>
+	<div>
+		<a href="personal.php">Мои баллы</a>
+	</div>
+	<div>
+		<a href="stud_ved.php">Рейтинг</a>
+	</div>
+	<div>
+		<a href="logout.php">Выйти</a>
+	</div>	
+</body>
+</html>
