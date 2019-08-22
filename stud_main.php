1285c9b4fdd67f6f2bd095f18057b9824c0884dc
linaShurygina <rocknrolllb20@gmail.com>
2019-08-23 01:28:21 +0300

    .

--- stud_main.php
+++ stud_main.php
@@ -11,13 +11,13 @@
 		<a href="messages.php">Сообщения</a>
 	</div>
 	<div>
-		<a href="purposes.php">Цели</a>
+		<a href="stud_purposes.php">Цели</a>
 	</div>
 	<div>
-		<a href="personal.php">Мои баллы</a>
+		<a href="stud_personal.php">Мои баллы</a>
 	</div>
 	<div>
-		<a href="stud_ved.php">Рейтинг</a>
+		<a href="stud_rating.php">Рейтинг</a>
 	</div>
 	<div>
 		<a href="logout.php">Выйти</a>
