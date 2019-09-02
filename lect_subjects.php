10371109bf05b85a865518e01721cf74d9d8631e
AlexanderChirkin <chalex2000@yandex.ru>
2019-09-03 00:49:03 +0300

    Разбил на функции, объединил sql запросы, вынес начало страницы в другой файл.

--- lect_subjects.php
+++ lect_subjects.php
@@ -1,117 +1,100 @@
-<?php      					 
-session_start();				
-if (isset($_SESSION['username'])) // если установлена сессия
-{
-	$useremail = $_SESSION['username'];
-	require_once 'login.php';
-	$connection = new mysqli($hostname, $username, $password, $database);
-	if ($connection->connect_error) die("Fatal Error");
-	
-	echo "$useremail <br>";
-	$query   = "SELECT * FROM user WHERE email = '$useremail'" ;
-    $result  = $connection->query($query);
-    if (!$result) die("User not found");
-    
-	elseif ($result->num_rows)  // если нашли юзера с таким имейлом
-    {
-		$users_count = $result->num_rows;
-		$user = $result->fetch_array(MYSQLI_NUM);
+<?php 
+// добавить всплывающие уведомления, выбор одного или нескольких предметов в добавлении , вместо кнопок удалить нужно использовать иконки корзины   					 
+require_once 'header.php';
+require_once 'header_lect.php';  // проверка роли(преподаватель)
 
-		$result->close();
-		if ($user[5] == 'lecturer')
+if (isset($_POST['add']))
+	add_subjects($user_email);
+$subjects = get_users_subjects($user_email, $connection);
+if (delete_subjects($subjects, $user_email))
+	$subjects = get_users_subjects($user_email, $connection);			
+echo <<<_MAIN
+		<main>
+        <div class='lect-subjects'>
+		<form method="POST" action="lect_subjects.php">
+_MAIN;
+print_subjects($subjects);
+print_add();
+echo "</form> </div> </main> </body> </html>";
+
+
+function get_users_subjects($user_email)
+{
+	global $connection;
+	$subjects = array();
+	$query = "SELECT id, name 
+				FROM subject 
+				WHERE id IN
+					(SELECT subject_id 
+						FROM lecturer_subject 
+						WHERE lecturer_id = '$user_email')
+				ORDER BY name";                            // не сортируются они по алфавиту
+	$result = $connection->query($query);
+	if (! $result) die ("$connection -> connect_error"); // нормальное сообщение об ошибке и завршение программы
+	$result -> data_seek(0);
+	while ($row = $result -> fetch_array(MYSQLI_ASSOC))
+		$subjects[$row['id']] = $row['name'];
+	return $subjects;
+}
+
+function add_subjects($useremail)
+{
+	global $connection;
+	$subj_id = $_POST['subject'];
+	$query  = "SELECT * FROM lecturer_subject WHERE lecturer_id = '$useremail' AND subject_id = '$subj_id'";
+	$result = $connection->query($query);
+	if (!$result) die ("$connection->connect_error");
+	$rows = $result->num_rows;
+	if ($rows == 0)
+	{
+		$query  = "INSERT INTO lecturer_subject VALUES('$useremail', '$subj_id')";
+		$result = $connection->query($query);
+		if (!$result) die ("$connection->connect_error");
+		echo "<br> Уведомление : Предмет добавлен <br>";
+	}
+		else echo "<br> Уведомление : предмет существует <br>";
+}
+
+function delete_subjects($subjects, $useremail)
+{
+	global $connection;
+	$flag = False;
+	foreach ($subjects as $id => $name)
+	{
+		if (isset($_POST["$id"])) 
 		{
-			
-			if (isset($_POST['add']))
-			{
-				$subj_id = $_POST['subject'];
-				$query  = "SELECT * FROM lecturer_subject WHERE lecturer_id = '$useremail' AND subject_id = '$subj_id'";
-				$result = $connection->query($query);
-				if (!$result) die ("Database access failed");
-				$rows = $result->num_rows;
-				if ($rows == 0)
-				{
-					$query  = "INSERT INTO lecturer_subject VALUES('$useremail', '$subj_id')";
-					$result = $connection->query($query);
-					if (!$result) die ("Database access failed");
-					echo "Предмет добавлен";
-				}
-				else echo "предмет существует";
-			}
-			
-			echo '<form method="POST" action="lect_subjects.php">';
-			$subjects = get_subjects($useremail, $connection);
-			$flag = False;
-			foreach ($subjects as $id => $name)
-			{
-				if (isset($_POST["$id"])) 
-				{
-				$query  = "DELETE FROM lecturer_subject WHERE lecturer_id = '$useremail' AND subject_id = '$id' ";
-				$result = $connection->query($query);
-				if (!$result) die ("Database access failed 2");
-				$flag = True;
-				echo("$id");
-				}
-			}
-			if ($flag)
-				$subjects = get_subjects($useremail, $connection);
-			print_subjects($subjects);
-			echo(" Добавить <br>");
-			echo ("<select name = 'subject' size = '1' >");
-			$query  = "SELECT * FROM subject ";
+			$query  = "DELETE FROM lecturer_subject WHERE lecturer_id = '$useremail' AND subject_id = '$id' ";
 			$result = $connection->query($query);
-			if (!$result) die ("Database access failed");
-			$rows = $result->num_rows;
-			for ($j = 0 ; $j < $rows ; ++$j)
-			{
-				$result->data_seek($j);
-				$subject = $result->fetch_array(MYSQLI_NUM);
-				echo "<option value = "."$subject[0]".">"."$subject[1]"." </option>";
-			}
-			echo "</select>";
-			echo '<br> <input type="submit" name="add" value="Добавить">';
-			echo "</form>";
-			
-			
-			// обработать добавление и удаление предметов 
-			
-			
-			
-			
-			
-			
+			if (!$result) die ("$connection->connect_error");
+			$flag = True;
+			echo("<br> Уведомление: предмет удалён $id <br>");
 		}
 	}
-}
-else 
-	echo "Please <a href=authentication.html>click here</a> to log in.";
-
-
-function get_subjects($useremail, $connection)
-{
-	$subjects = array();
-	$query  = "SELECT * FROM lecturer_subject WHERE lecturer_id = '$useremail'";
-			$result = $connection->query($query);
-			if (!$result) die ("Database access failed");
-			$rows = $result->num_rows;
-			for ($j = 0 ; $j < $rows ; ++$j)
-			{
-				$result->data_seek($j);
-				$lect_subj = $result->fetch_array(MYSQLI_NUM);
-				
-				$query2 = " SELECT * FROM subject WHERE id = $lect_subj[1]";
-				$result2 = $connection->query($query2);
-				if (!$result2) die ("Database access failed  2");
-				$subj = $result2->fetch_array(MYSQLI_NUM);
-				$subjects["$lect_subj[1]"] = $subj[1];
-			}
-	return $subjects;
+	return $flag;
 }
 
 function print_subjects($subjects)
 {
 	foreach ($subjects as $id => $name)
 	{
-		echo ("$name <input type='submit' name='$id' value='удалить'> <br>");
+		echo ("<div class = 'subject'> $name <input type='submit' name='$id' value='Удалить'> </div>");
 	}
+}
+
+function print_add()
+{
+	global $connection;
+	//echo(" Добавить <br>");
+			echo ("<div class = 'add-subject'> <select name = 'subject' size = '1' > ");
+			$query  = "SELECT id, name FROM subject ORDER BY name";
+			$result = $connection->query($query);
+			if (!$result) die ("$connection->connect_error");
+			$result->data_seek(0);
+			while ($row = $result->fetch_array(MYSQLI_ASSOC))
+			{
+				echo "<option value = ".$row['id'].">".$row['name']." </option>";
+			}
+			echo "</select>";
+			echo '<input type="submit" name="add" value="Добавить"> </div>';
 }
  ?>
