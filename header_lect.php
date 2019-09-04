6490c4a355e3f3d8ff9936ad4c973a97d12cecab
AlexanderChirkin <chalex2000@yandex.ru>
2019-09-04 10:52:24 +0300

    Кнопка "выйти", ссылки на страницы.

--- header_lect.php
+++ header_lect.php
@@ -1,18 +1,19 @@
 <?php
-require_once 'check_access.php';
-$res = check_role($connection, $user_email); 
-if ($res == 'student') // студент пытается взломать нас
+require_once("head.php");
+if ($who == 'student') // студент пытается взломать нас
 	echo "Students have not access to tis page";
-else if ($res == -1)
+else if ($who == 'guest')
 	echo "error"; // ошибка, завершение программы
 
 echo <<<_HEADER
-    
+    <body>
+	<header>
 	<div class="navigation">
-		<a class="nav-item" href="#">Ведомость</a> 
-		<a class="nav-item" href="#">Статистика</a> 
-		<a class="nav-item" href="#">Мои предметы</a>
+		<a class="nav-item" href="lect_ved.php">Ведомость</a> 
+		<a class="nav-item" href="lect_rating.php">Статистика</a> 
+		<a class="nav-item" href="lect_subjects.php">Мои предметы</a>
 		<a class="nav-item" href="#">Сообщения</a>
+		<a class="nav-item" href="logout.php">Выйти</a>
 	</div>
 	</header>
 _HEADER;
