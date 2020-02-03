536e7cdc76d32371749dc782c91c2b37c14a1aa8
linaShurygina <rocknrolllb20@gmail.com>
2020-02-03 19:16:28 +0300

    Проверка ошибок при вводе

--- js/validate_forms.js
+++ js/validate_forms.js
@@ -1,11 +1,26 @@
 function validate(form) {
-    fail = validateEmail(form.email.value)
+    fail = validateEmail(form.email.value);
+    fail += validatePassword(form.password.value);
+    if (fail == "") return true;
+    else { alert(fail); return false }
+}
+function validateReg(form) {
+    fail = validateEmail(form.email.value);
+    fail += validateRole(form.role.value);
     if (fail == "") return true
     else { alert(fail); return false }
 }
 function validateEmail(field) {
-    if (field == "") return "Пропущено поле"
+    if (field == "") return "Введите логин"
     else if ((field.indexOf("@") < 0) || (field.indexOf(".") < 0))
         return "Введите email в верном формате"
     return ""
+}
+
+function validatePassword(field) {
+    if (field == "") return "Введите пароль";
+}
+
+function validateRole(selector) {
+    if (selector == "") return "Вы кто (who)";
 }
--- registration.php
+++ registration.php
@@ -1,8 +1,73 @@
 <?php
     $title = "Регистрация";
     require_once 'head.php';
-    $error = '';
+	require_once 'login.php';
+	require_once 'encrypt.php';
+	require_once 'functions.php';
+	$connection = new mysqli($hostname, $username, $password, $database);
+	if ($connection->connect_error) die($connection->connect_error);
+
+	query_mysql($connection, "SET NAMES utf8");
+
+	$error = $name = $surname = $midname = $email = $password = $role = "";
+	#$semester = $group = NULL;
+
+	if (isset($_POST['name']) &&
+		isset($_POST['surname']) &&
+		isset($_POST['midname']) &&
+		isset($_POST['email']) &&
+		isset($_POST['password']) &&
+        isset($_POST['role']))
+	{
+		$name = sanitize_string($connection, $_POST['name']);
+		$surname = sanitize_string($connection, $_POST['surname']);
+		$midname = sanitize_string($connection, $_POST['midname']);
+		$email = sanitize_string($connection, $_POST['email']);
+		$password = sanitize_string($connection, $_POST['password']);
+		$role = $_POST['role'];
+
+		$token = new_token($password);
+
+		if (isset($_POST['semester']) && isset($_POST['group']))
+		{
+			$group = $_POST['group'];
+			if (date('d/m') < '01/02')
+            {
+                $semester = $_POST['semester'] * 2 - 1;
+            }
+			else
+            {
+                $semester = $_POST['semester'] * 2;
+            }
+		}
+
+		$result = query_mysql($connection, "SELECT * FROM user WHERE email='$email'");
+		if ($result->num_rows)
+			$error = "Этот e-mail уже зарегистрирован";
+		else
+		{
+			query_mysql($connection, "INSERT INTO user(email, password, surname, name, middle_name, role) VALUES('$email', '$token',
+															 '$surname', '$name',
+															 '$midname', '$role')");
+			if ($role == 'student')
+			{
+				$result = query_mysql($connection, "SELECT id FROM user WHERE email='$email'");
+				if (!$result) die ("Пользователь не найден");
+				else
+				{
+					$row = $result->fetch_array(MYSQLI_ASSOC);
+					$id = $row['id'];
+					query_mysql($connection, "INSERT INTO student VALUES('$id', '$semester', '$group')");
+				}
+			}
+			header("Location: index.php");
+		}
+	}
+
+	$connection->close();
+
 ?>
+<script src="js/validate_forms.js"></script>
 <script>
     /*
     var roleList = document.getElementsByName('role');
@@ -25,39 +90,39 @@
 </script>
 <body class="auth">
     <div class="reg">
-        <form method="post" action="registration.php">
+        <form method="post" action="registration.php" onsubmit="return validateReg(this)">
             <div class="errors">
                 <?php echo $error ?>
             </div>
             <div class="str-input-reg">
                 <label for="surname">Фамилия</label>
-                <input type="text" name="surname" maxlength="32">
+                <input type="text" name="surname" maxlength="32" required value=" <?php echo $surname ?> ">
             </div>
             <div class="str-input-reg">
                 <label for="name">Имя</label>
-                <input type="text" name="name" maxlength="32">
+                <input type="text" name="name" maxlength="32" required value="<?php echo $name ?>">
             </div>
             <div class="str-input-reg">
                 <label for="midname">Отчество</label>
-                <input type="text" name="midname" maxlength="32">
+                <input type="text" name="midname" maxlength="32" required value="<?php echo $midname ?>">
             </div>
             <div class="str-input-reg">
                 <label for="email">E-mail</label>
-                <input type="email" name="email" maxlength="50">
+                <input type="email" name="email" maxlength="50" required value="<?php echo $email ?>">
             </div>
             <div class="str-input-reg">
                 <label for="password">Пароль</label>
-                <input type="password" name="password" maxlength="50">
+                <input type="password" name="password" maxlength="50" required>
             </div>
             <div class="list" id="role-list">
-                <select name="role" size="1" onchange="showSelectorsForStudent(this.value)">
+                <select name="role" size="1" required onchange="showSelectorsForStudent(this.value)">
                     <option selected>Выберите...</option>
                     <option value="lecturer">Преподаватель</option>
                     <option value="student">Студент</option>
                 </select>
             </div>
             <div class="list" id="semester">
-                <select name="semester" size="1">
+                <select name="semester" size="1" required>
                     <option selected>Курс...</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -66,7 +131,7 @@
                 </select>
             </div>
             <div class="list" id="group">
-                <select name="group" size="1">
+                <select name="group" size="1" required>
                     <option selected>Группа...</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -81,64 +146,3 @@
         </form>
     </div>
 </body>
-
-<?php
-	require_once 'login.php';
-	require_once 'encrypt.php';
-	require_once 'functions.php';
-	$connection = new mysqli($hostname, $username, $password, $database);
-	if ($connection->connect_error) die($connection->connect_error);
-
-	query_mysql($connection, "SET NAMES utf8");
-
-	$error = $name = $surname = $midname = $email = $password = $role = "";
-	#$semester = $group = NULL;
-
-	if (isset($_POST['name']) &&
-		isset($_POST['surname']) &&
-		isset($_POST['midname']) &&
-		isset($_POST['email']) &&
-		isset($_POST['password']))
-	{
-		$name = sanitize_string($connection, $_POST['name']);
-		$surname = sanitize_string($connection, $_POST['surname']);
-		$midname = sanitize_string($connection, $_POST['midname']);
-		$email = sanitize_string($connection, $_POST['email']);
-		$password = sanitize_string($connection, $_POST['password']);
-		$role = $_POST['role'];
-
-		$token = new_token($password);
-
-		if (isset($_POST['semester']) && isset($_POST['group']))
-		{
-			$semester = $_POST['semester'];
-			$group = $_POST['group'];
-		}
-
-		$result = query_mysql($connection, "SELECT * FROM user WHERE email='$email'");
-		if ($result->num_rows)
-			echo $error = "Этот e-mail уже зарегистрирован<br><br>";
-		else
-		{
-			query_mysql($connection, "INSERT INTO user(email, password, surname, name, middle_name, role) VALUES('$email', '$token',
-															 '$surname', '$name',
-															 '$midname', '$role')");
-			if ($role == 'student')
-			{
-				$result = query_mysql($connection, "SELECT id FROM user WHERE email='$email'");
-				if (!$result) die ("Пользователь не найден");
-				else
-				{
-					$row = $result->fetch_array(MYSQLI_ASSOC);
-					$id = $row['id'];
-					query_mysql($connection, "INSERT INTO student VALUES('$id', '$semester', '$group')");
-				}
-			}
-
-			die("Аккаунт успешно создан.<p><a href='continue.php'>Войти на сайт</a></p>");
-		}
-	}
-
-	$connection->close();
-
-?>
