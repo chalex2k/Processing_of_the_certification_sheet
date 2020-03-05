3e34a0bb7f9bcd7d9387c549e68381ab0d82d1fc
linaShurygina <rocknrolllb20@gmail.com>
2020-03-05 13:03:15 +0300

    VC-refactor: registration

---
+++ controllers/registration.php
@@ -0,0 +1,68 @@
+<?php
+    require_once "render.php";
+    require_once "start.php";
+	require_once '../service/encrypt.php';
+
+	try {
+
+        $error = $name = $surname = $midname = $email = $password = $role = "";
+
+        if (isset($_POST['name']) &&
+            isset($_POST['surname']) &&
+            isset($_POST['midname']) &&
+            isset($_POST['email']) &&
+            isset($_POST['password']) &&
+            isset($_POST['role'])) {
+            $name = sanitize_string($connection, $_POST['name']);
+            $surname = sanitize_string($connection, $_POST['surname']);
+            $midname = sanitize_string($connection, $_POST['midname']);
+            $email = sanitize_string($connection, $_POST['email']);
+            $password = sanitize_string($connection, $_POST['password']);
+            $role = $_POST['role'];
+
+            $token = new_token($password);
+
+            if (isset($_POST['semester']) && isset($_POST['group'])) {
+                $group = $_POST['group'];
+                if (date('d/m') < '01/02') {
+                    $semester = $_POST['semester'] * 2 - 1;
+                } else {
+                    $semester = $_POST['semester'] * 2;
+                }
+            }
+
+            $result = query_mysql($connection, "SELECT * FROM user WHERE email='$email'");
+            if ($result->num_rows)
+                $error = "Этот e-mail уже зарегистрирован";
+            else {
+                query_mysql($connection, "INSERT INTO user(email, password, surname, name, middle_name, role) VALUES('$email', '$token',
+															 '$surname', '$name',
+															 '$midname', '$role')");
+                if ($role == 'student') {
+                    $result = query_mysql($connection, "SELECT id FROM user WHERE email='$email'");
+                    if (!$result) die ("Пользователь не найден");
+                    else {
+                        $row = $result->fetch_array(MYSQLI_ASSOC);
+                        $id = $row['id'];
+                        query_mysql($connection, "INSERT INTO student VALUES('$id', '$semester', '$group')");
+                    }
+                }
+                header("Location: index.php");
+            }
+        }
+        $connection->close();
+
+        return render('registration',
+            [   'title' => 'Регистрация',
+                'userstr' => $userstr,
+                'error' => $error,
+                'surname' => $surname,
+                'name' => $name,
+                'midname' => $midname,
+                'email' => $email   ] );
+    }
+    catch (Exception $exc) {
+        return render('error', []);
+    }
+
+?>
--- views/registration.php
+++ views/registration.php
@@ -1,82 +1,12 @@
-<?php
-    $title = "Регистрация";
-    require_once 'head.php';
-	require_once 'login.php';
-	require_once 'encrypt.php';
-	require_once 'functions.php';
-	$connection = new mysqli($hostname, $username, $password, $database);
-	if ($connection->connect_error) die($connection->connect_error);
-
-	query_mysql($connection, "SET NAMES utf8");
-
-	$error = $name = $surname = $midname = $email = $password = $role = "";
-	#$semester = $group = NULL;
-
-	if (isset($_POST['name']) &&
-		isset($_POST['surname']) &&
-		isset($_POST['midname']) &&
-		isset($_POST['email']) &&
-		isset($_POST['password']) &&
-        isset($_POST['role']))
-	{
-		$name = sanitize_string($connection, $_POST['name']);
-		$surname = sanitize_string($connection, $_POST['surname']);
-		$midname = sanitize_string($connection, $_POST['midname']);
-		$email = sanitize_string($connection, $_POST['email']);
-		$password = sanitize_string($connection, $_POST['password']);
-		$role = $_POST['role'];
-
-		$token = new_token($password);
-
-		if (isset($_POST['semester']) && isset($_POST['group']))
-		{
-			$group = $_POST['group'];
-			if (date('d/m') < '01/02')
-            {
-                $semester = $_POST['semester'] * 2 - 1;
-            }
-			else
-            {
-                $semester = $_POST['semester'] * 2;
-            }
-		}
-
-		$result = query_mysql($connection, "SELECT * FROM user WHERE email='$email'");
-		if ($result->num_rows)
-			$error = "Этот e-mail уже зарегистрирован";
-		else
-		{
-			query_mysql($connection, "INSERT INTO user(email, password, surname, name, middle_name, role) VALUES('$email', '$token',
-															 '$surname', '$name',
-															 '$midname', '$role')");
-			if ($role == 'student')
-			{
-				$result = query_mysql($connection, "SELECT id FROM user WHERE email='$email'");
-				if (!$result) die ("Пользователь не найден");
-				else
-				{
-					$row = $result->fetch_array(MYSQLI_ASSOC);
-					$id = $row['id'];
-					query_mysql($connection, "INSERT INTO student VALUES('$id', '$semester', '$group')");
-				}
-			}
-			header("Location: index.php");
-		}
-	}
-
-	$connection->close();
-
-?>
-<!--<script src="js/validate_forms.js"></script>-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title> <?php echo " $title $userstr " ?> </title>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
 <script>
-    /*
-    var roleList = document.getElementsByName('role');
-    roleList.onclick = function () {
-        if (roleList.value === "student") {
-            document.getElementsByName('semester').style.display = 'block';
-            document.getElementsByName('group').style.display = 'block';
-        }
-    }*/
     function showSelectorsForStudent(value) {
         if (value == 'student') {
             document.getElementById('semester').style.display = 'block';
